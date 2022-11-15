e3be58ec7357e1b09752e0109f70af2a0fa452a2
Marlon Cuevas <marloncuevas1@gmail.com>
2022-11-16 01:26:30 +0800

    login state changes

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,7 +3,7 @@ import { AuthService } from '@auth0/auth0-angular';
 import { mergeMap } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Browser } from '@capacitor/browser';
-import { CapacitorConfig }from '../../../capacitor.config';
+import { CapacitorConfig } from '../../../capacitor.config';
 
 // const returnTo = `${CapacitorConfig.appId}://dev-imaging-app.us.auth0.com/capacitor/${CapacitorConfig.appId}/callback`;
 const returnTo = 'https://localhost:4200'
@@ -15,17 +15,25 @@ const returnTo = 'https://localhost:4200'
 })
 export class LoginPage implements OnInit {
 
+  isLoggedIn = false;
   constructor(public auth: AuthService) { }
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {
+    this.auth.user$.subscribe(res => {
+      if (res){
+        this.isLoggedIn = true;
+      }
+    });
   }
+
 
   login() {
     this.auth
       .buildAuthorizeUrl()
       .pipe(mergeMap((url) => Browser.open({ url, windowName: '_self' })))
       .subscribe();
+    this.isLoggedIn = true;
   }
 
   logout() {
@@ -37,7 +45,9 @@ export class LoginPage implements OnInit {
           // Call the logout fuction, but only log out locally
           this.auth.logout({ localOnly: false });
           // Redirect to Auth0 using the Browser plugin, to clear the user's session
-          sessionStorage.clear();  
+          this.isLoggedIn = false;
+          sessionStorage.clear();
+          Browser.open({ url, windowName: '_self' })
           // Browser.open({ url });
         })
       )
